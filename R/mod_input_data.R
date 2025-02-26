7506b24f0ff2314bca19ab8ef90957aad0a041d1
Weronika Puchała <ciemnamateria@gmail.com>
2025-02-26 12:02:40 +0100

    setting the repo

---
+++ R/mod_input_data.R
@@ -0,0 +1,112 @@
+#' input_data UI Function
+#'
+#' @description A shiny Module.
+#'
+#' @param id,input,output,session Internal parameters for {shiny}.
+#'
+#' @noRd 
+#'
+#' @importFrom shiny NS tagList 
+mod_input_data_ui <- function(id) {
+  ns <- NS(id)
+  tagList(
+    wellPanel(
+      fillRow(
+        flex = c(NA, 1),
+        fileInput(
+          inputId = ns("data_file"),
+          label = "Choose file:",
+          multiple = FALSE,
+          accept = c(".csv", ".xlsx", ".xls"),
+          placeholder = "No file selected"
+        ),
+        div(
+          id = "HaDeX-file-status-panel",
+          h6("File status:"),
+          div(
+            id = "HaDeX-file-status-message",
+            verbatimTextOutput(ns("data_file_info"))
+          )
+        )
+      )
+    )
+ 
+  )
+}
+    
+#' input_data Server Functions
+#'
+#' @noRd 
+mod_input_data_server <- function(id){
+  moduleServer(id, function(input, output, session){
+    ns <- session$ns
+ 
+    dat_raw <- reactive({
+      data_file <- input[["data_file"]]
+      
+      if (is.null(data_file)) {
+        example_data_alpha
+      } else {
+        validate(need(try({
+          file <- HaDeX::read_hdx(data_file[["datapath"]])
+        }), "File does not fullfill requirements. Check file requirements!"))
+        file
+      }
+    })
+    
+    data_source <- reactive({ attr(dat_raw(), "source") })
+    
+    dat_adjusted <- reactive({
+             dat_raw()
+    })
+    
+    ### other outputs
+    
+    output[["data_file_info"]] <- renderText({
+      paste0(
+        if (is.null(input[["data_file"]]))
+          "Example file: eEF1B_alpha.csv."
+        else "Supplied file is valid.",
+        "\nDetected data source: ", data_source(),
+        if (data_source() == "HDeXaminer")
+          ". User action needed below!"
+        else "."
+      )
+    })
+    
+    ### observer
+    
+    observe({
+      toggle_id(data_source() == "HDeXaminer", "HaDeX-examiner-settings-panel")
+    })
+    
+    ### return values
+    
+    return(
+      c(
+        dat,
+        str_path = str_path,
+        list(input_info = reactive({
+          data_file <- input[["data_file"]]
+          
+          if (is.null(data_file)) {
+            list(is_example = TRUE)
+          } else {
+            list(
+              is_example = FALSE,
+              name = data_file[["name"]],
+              hash = tools::md5sum(as.character(data_file[["datapath"]]))
+            )
+          }
+          
+        }))
+      )
+    )
+  })
+}
+    
+## To be copied in the UI
+# mod_input_data_ui("input_data_1")
+    
+## To be copied in the server
+# mod_input_data_server("input_data_1")
--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# splitteR
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# `{splitteR}`
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+## Installation
+
+You can install the development version of `{splitteR}` like so:
+
+``` r
+# FILL THIS IN! HOW CAN PEOPLE INSTALL YOUR DEV PACKAGE?
+```
+
+## Run
+
+You can launch the application by running:
+
+``` r
+splitteR::run_app()
+```
+
+## About
+
+You are reading the doc about version : 0.0.0.9000
+
+This README has been compiled on the
+
+``` r
+Sys.time()
+#> [1] "2025-02-26 11:55:40 CET"
+```
+
+Here are the tests results and package coverage:
+
+``` r
+devtools::check(quiet = TRUE)
+#> ℹ Loading splitteR
+#> ── R CMD check results ──────────────────────────────── splitteR 0.0.0.9000 ────
+#> Duration: 32.6s
+#> 
+#> ❯ checking for future file timestamps ... NOTE
+#>   unable to verify current time
+#> 
+#> ❯ checking dependencies in R code ... NOTE
+#>   import '::' lub ':::' nie jest zadeklarowany z: 'HaDeX'
+#> 
+#> ❯ checking R code for possible problems ... NOTE
+#>   mod_input_data_server : <anonymous>: no visible binding for global
+#>     variable 'example_data_alpha'
+#>   mod_input_data_server : <anonymous>: no visible global function
+#>     definition for 'toggle_id'
+#>   mod_input_data_server : <anonymous>: no visible binding for global
+#>     variable 'dat'
+#>   mod_input_data_server : <anonymous>: no visible binding for global
+#>     variable 'str_path'
+#>   Undefined global functions or variables:
+#>     dat example_data_alpha str_path toggle_id
+#> 
+#> 0 errors ✔ | 0 warnings ✔ | 3 notes ✖
+```
+
+``` r
+covr::package_coverage()
+#> Error in loadNamespace(x): nie ma pakietu o nazwie 'covr'
+```
