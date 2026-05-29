7d30ab84540ae11566f1d637ad864916b01c6085
Weronika Puchała <ciemnamateria@gmail.com>
2026-05-29 14:22:45 +0200

    changed uc plotting function

--- R/plot_uc_with_origin.R
+++ R/plot_uc_with_origin.R
@@ -33,31 +33,36 @@ plot_uc_with_origin <- function(dat,
                                                 states = state)
   
   sub_kin_dat <- calculate_peptide_kinetics(subsection_dat)
+  
   sub_max_uptake <- stringr::str_count(unique(sub_kin_dat[["Sequence"]]), "[A-Z]")
   
   common <- subsection[["common"]]
+  
+  longer_sequence <- unique(longer_kin_dat[["Sequence"]])
+  shorter_sequence <- unique(shorter_kin_dat[["Sequence"]])
+  sub_sequence <- unique(sub_kin_dat[["Sequence"]])
                                        
   low_y <- min(min(sub_kin_dat[["deut_frac"]]), 0)
     
     ggplot() +
-      geom_point(dat = longer_kin_dat, aes(x = Exposure, y = deut_uptake, color = Sequence)) +
-      geom_line(dat = longer_kin_dat, aes(x = Exposure, y = deut_uptake, color = Sequence)) +
+      geom_point(dat = longer_kin_dat, aes(x = Exposure, y = deut_uptake, color = longer_sequence)) +
+      geom_line(dat = longer_kin_dat, aes(x = Exposure, y = deut_uptake, color = longer_sequence)) +
       geom_ribbon(dat = longer_kin_dat, aes(x = Exposure, 
                                             ymin = deut_uptake - err_deut_uptake,
                                             ymax = deut_uptake + err_deut_uptake,
-                                            fill = Sequence),  alpha = 0.15) + 
-      geom_point(dat = shorter_kin_dat, aes(x = Exposure, y = deut_uptake, color = Sequence)) +
-      geom_line(dat = shorter_kin_dat, aes(x = Exposure, y = deut_uptake, color = Sequence)) +
+                                            fill = longer_sequence),  alpha = 0.15) + 
+      geom_point(dat = shorter_kin_dat, aes(x = Exposure, y = deut_uptake, color = shorter_sequence)) +
+      geom_line(dat = shorter_kin_dat, aes(x = Exposure, y = deut_uptake, color = shorter_sequence)) +
       geom_ribbon(dat = shorter_kin_dat, aes(x = Exposure, 
                                             ymin = deut_uptake - err_deut_uptake,
                                             ymax = deut_uptake + err_deut_uptake,
-                                            fill = Sequence),  alpha = 0.15) +   
-      geom_point(dat = sub_kin_dat, aes(x = Exposure, y = deut_uptake, color = Sequence)) +
-      geom_line(dat = sub_kin_dat, aes(x = Exposure, y = deut_uptake, color = Sequence), linetype = 2) +
+                                            fill = shorter_sequence),  alpha = 0.15) +   
+      geom_point(dat = sub_kin_dat, aes(x = Exposure, y = deut_uptake, color = sub_sequence)) +
+      geom_line(dat = sub_kin_dat, aes(x = Exposure, y = deut_uptake, color = sub_sequence), linetype = 2) +
       geom_ribbon(dat = sub_kin_dat, aes(x = Exposure, 
                                             ymin = deut_uptake - err_deut_uptake,
                                             ymax = deut_uptake + err_deut_uptake,
-                                            fill = Sequence),  alpha = 0.15) + 
+                                            fill = sub_sequence),  alpha = 0.15) + 
       scale_x_log10() + 
       ylim(c(low_y-1, NA)) +
       geom_hline(yintercept = 0, linetype = "dotted", size = 0.25) +
@@ -65,7 +70,21 @@ plot_uc_with_origin <- function(dat,
       theme(legend.position = "bottom") +
       labs(title = paste0("Deuterium uptake for subsection ", sub_kin_dat[["Sequence"]][1], " and the original peptides"),
            x = "Exposure [log(min)]",
-           y = "Deuterium uptake [Da]") 
+           y = "Deuterium uptake [Da]",
+           color = "") +
+      scale_color_manual(
+        values = c(
+          setNames("#F8766D", longer_sequence),
+          setNames("#00BA38", shorter_sequence),
+          setNames("#619CFF", sub_sequence)),
+        breaks = c(longer_sequence, shorter_sequence, sub_sequence)) +
+      scale_fill_manual(
+        values = c(
+          setNames("#F8766D", longer_sequence),
+          setNames("#00BA38", shorter_sequence),
+          setNames("#619CFF", sub_sequence))) +
+      guides(fill = "none")
+   
 
       
  
